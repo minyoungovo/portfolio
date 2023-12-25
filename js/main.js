3e6b823930f826c145807d01b1b59fe45c0f2fe6
minyoungovo <minyoungovo@gmail.com>
2023-12-26 00:42:42 +0900

    section, footer 수정

--- js/main.js
+++ js/main.js
@@ -37,5 +37,18 @@ $(function () {
     //     }
     // });
 
+    //스크롤 시 애니메이션 효과
+    $(window).scroll( function(){
+        /* 2 */
+        $('.hideme').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* 3 */
+            if( bottom_of_window > bottom_of_object/2 ){
+                $(this).animate({'opacity':'1'},500);
+            }
+        }); 
+    });
+
 
 });
