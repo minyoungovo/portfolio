77d8e18fbc09b86af38093fde0fe5273d12a5299
minyoungovo <minyoungovo@gmail.com>
2024-05-01 17:43:49 +0900

    상단으로 이동 버튼 수정(최상위 일 때 노출x), contact us 수정

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,39 @@
 $(document).ready(function() {
+    let scrollHeight = 0;
+
+    //새로고침 시 스크롤 위치 지정
+    const scrollY = parseInt(sessionStorage.getItem("scrollY"));
+
+    if(scrollY && scrollY > 250){
+        window.scrollTo(0, scrollY);
+        $('#top_btn').css('visibility', 'visible');
+    }
+
     const counters = $(".scroll_on");
     
     const exposurePercentage = 100; 
     const loop = true; 
 
+    // 버튼 클릭시 
+    $("#top_btn").click(function() { 
+        $('html, body').animate({ scrollTop : 0 }, 400); 
+    }); 
     
     $(window).on('scroll', function() {
+
+        if ($(this).scrollTop() > 250) { 
+            $('#top_btn').css('visibility', 'visible');
+        } else { 
+            $('#top_btn').css('visibility', 'hidden');
+        } 
+
+
         counters.each(function() {
+
+            //세션 스토리지에 저장하여 새로고침 시 현재 스크롤 위치 값 반환
+            scrollHeight = $(document).scrollTop();
+            sessionStorage.setItem("scrollY", scrollHeight);
+
             const $el = $(this);
     
             const rect = $el[0].getBoundingClientRect();
@@ -22,7 +49,4 @@ $(document).ready(function() {
         });
     }).scroll();
 
-
-
-   
 });
